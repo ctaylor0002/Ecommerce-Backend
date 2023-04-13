48bde33f0b14657fec8f0bb07cdf3f775a6cc571
ctaylor0002 <charles4312@comcast.net>
2023-04-13 00:18:17 -0400

    Established MySQL Connection + Made Basic Django Migrations To Database

--- ecommerce_backend/ecommerce_backend/settings.py
+++ ecommerce_backend/ecommerce_backend/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(g%m4dgc$xr*je4gmccz9p3j^%0obfe^o-c3+yn9xh=%pm_a^v'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +72,7 @@ WSGI_APPLICATION = 'ecommerce_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
@@ -121,3 +115,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from ecommerce_backend.local_settings import *
+except ImportError:
+    pass
